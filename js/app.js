0492caab478cb085dead43d10f5220cfe77edee0
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-04-08 13:52:18 +0200

    Added: method generateEmails()

--- js/app.js
+++ js/app.js
@@ -4,6 +4,22 @@ const {createApp} = Vue
 
 createApp({
     data(){
-        return{}
+        return{
+            emails: []
+        }
+    },
+    methods:{
+        generateEmails(){
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) =>{
+                    console.log(response.data.response)
+                    this.emails.push(response.data.response)
+                    console.log(this.emails)
+                })
+        }
+    },
+    created(){
+        this.generateEmails()
     }
 }).mount('#app')
