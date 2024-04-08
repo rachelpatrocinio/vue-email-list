3a6c9a2936fa67e3bb056d78d92dc432ff0d81ee
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-04-08 14:21:38 +0200

    Added: generateBumbers()

--- js/app.js
+++ js/app.js
@@ -5,23 +5,37 @@ const {createApp} = Vue
 createApp({
     data(){
         return{
-            emails: []
+            emails: [],
+            numbers: []
         }
     },
     methods:{
         generateEmails(nEmail){
             for(let i = 0; i < nEmail; i++){
                 axios
-                .get('https://flynn.boolean.careers/exercises/api/random/mail')
-                .then((response) =>{
-                    const generatedEmail = response.data.response
-                    this.emails.push(generatedEmail)
-                    // console.log(this.emails)
-                })
+                    .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((response) => {
+                        const generatedEmail = response.data.response
+                        this.emails.push(generatedEmail)
+                        // console.log(this.emails)
+                    })
             } 
+        },
+        generateNumbers(nNumbers){
+            for(let i = 0; i < nNumbers; i++){
+                axios
+                .get('https://flynn.boolean.careers/exercises/api/random/int')
+                .then((response) => {
+                    // console.log(response.data.response)
+                    const generatedNumber = response.data.response
+                    this.numbers.push(generatedNumber);
+                    console.log(this.numbers);
+                })
+            }
         }
     },
     created(){
         this.generateEmails(10)
+        this.generateNumbers(5)
     }
 }).mount('#app')
